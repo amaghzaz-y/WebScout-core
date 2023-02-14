03c33d3326e232e9f771702b681c25ed31b4ba0f
Yassine AMAGHZAZ <yassine.amaghzaz@outlook.com>
2023-02-14 13:42:21 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# WebScout
+🚧 WebScout is under development, and has not yet reached Alpha stage 🚧
+
+
+WebScout is a lightweight site search engine built using Rust and TypeScript that is optimized for serverless architecture and can run on the edge. Some of the key features of WebScout include:
+
+ - Search algorithm built on statistical and probability analysis that enables quick and accurate search through large amounts of documents
+ - Multiple indexes stored on KV for low latency retrieval
+ - Leveraging Cloudflare infrastructure for a serverless and distributed search
+ - Lightweight design that makes it easy to deploy and run on the edge
+ - WebScout is designed to work with Cloudflare Workers, using R2 Buckets for storage and KV for the index and caching layer. This allows for a scalable and reliable search solution that can be quickly and easily deployed.
+
+Overall, WebScout is an experimental search engine that's optimized for speed, reliability, and scalability. It's a great choice for anyone who needs a site search for low latency and wants a lightweight solution that can be easily deployed on the edge.
--- src/main.rs
+++ src/main.rs
@@ -11,29 +11,29 @@ use webscout::{
     query::Query,
     tokenizer::{self, Tokenizer},
 };
-fn serialize_docs() {
-    let dir = fs::read_dir("assets/books").unwrap();
-    let mut idx: Index = Index::new();
-    for file in dir {
-        let path = file.as_ref().unwrap().path().to_owned();
-        let name = file.as_ref().unwrap().file_name().into_string().unwrap();
-        let body = fs::read_to_string(path).unwrap();
-        println!("indexing {:}", name);
-        let doc: Document = Document::new(name.to_owned(), body, "en".to_string());
-        println!("adding document {:}", name);
-        idx.add_document(&doc);
-        println!("serializing {:}", name);
-        let content = doc.serialize();
-        println!("saving {:}", name);
-        fs::write(format!("packs/docs/{}.pack", name), content);
-    }
-    println!("saving index");
-    let mut cmp = GzEncoder::new(Vec::new(), Compression::default());
-    cmp.write_all(&idx.serialize());
-    let bin = cmp.finish().unwrap();
-    fs::write("packs/index/index.f2", bin);
-    fs::write("packs/index/index.pack", idx.serialize());
-}
+// fn serialize_docs() {
+//     let dir = fs::read_dir("assets/books").unwrap();
+//     let mut idx: Index = Index::new();
+//     for file in dir {
+//         let path = file.as_ref().unwrap().path().to_owned();
+//         let name = file.as_ref().unwrap().file_name().into_string().unwrap();
+//         let body = fs::read_to_string(path).unwrap();
+//         println!("indexing {:}", name);
+//         let doc: Document = Document::new(name.to_owned(), body, "en".to_string());
+//         println!("adding document {:}", name);
+//         idx.add_document(&doc);
+//         println!("serializing {:}", name);
+//         let content = doc.serialize();
+//         println!("saving {:}", name);
+//         fs::write(format!("packs/docs/{}.pack", name), content);
+//     }
+//     println!("saving index");
+//     let mut cmp = GzEncoder::new(Vec::new(), Compression::default());
+//     cmp.write_all(&idx.serialize());
+//     let bin = cmp.finish().unwrap();
+//     fs::write("packs/index/index.f2", bin);
+//     fs::write("packs/index/index.pack", idx.serialize());
+// }
 
 fn main() {
     //serialize_docs();
