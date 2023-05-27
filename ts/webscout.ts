347890aa1c668279e37b69357a4a1b7816f06012
Yassine AMAGHZAZ <yassine.amaghzaz@outlook.com>
2023-05-27 20:26:23 +0200

    error handling when index is null

--- ts/webscout.ts
+++ ts/webscout.ts
@@ -6,7 +6,7 @@ export default class WebScoutEngine {
 	constructor(index: Uint8Array, tokenizer: Uint8Array, language: string) {
 		initSync(wasm);
 		this.webscout = new WebScout(language);
-		this.webscout.deserialize_index(index);
+		if (index !== null) { this.webscout.deserialize_index(index); }
 		this.webscout.deserialize_tokenizer(tokenizer);
 		// sets up the query engine, optimizes memory
 		this.webscout.setup()
